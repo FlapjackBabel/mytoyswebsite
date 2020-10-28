471a2bf02edde45d334273c5f300288d10f181e1
FlapjackBabel <72535902+FlapjackBabel@users.noreply.github.com>
2020-10-28 15:22:22 +0700

    Update products.php

--- products.php
+++ products.php
@@ -49,6 +49,22 @@
 <!-- content -->
 <div class="wrapper row2">
   <div id="container" class="clear">
+	  <!-- Slider -->
+		<section id="slider" class="clear">
+		  <figure><img src="images/Background.jpg" alt="">
+			<figcaption>
+			  <h2>Introduction</h2>
+			  
+			  <?php foreach($list as $item) { ?>	
+			  
+			  <li class="oval"><a href="#"><img src="img\<?php echo $item["image"] ?> "width="144" height="144"/></a></li>
+			  
+				<?php } ?>
+				<?php } ?>
+				
+			</figcaption>
+		  </figure>
+		</section>
     <!-- main content -->
     <div id="intro">
       <section class="clear">
@@ -58,12 +74,8 @@
         <article class="two_quarter lastbox">
           <figure>
 			<ul class="clear">
-				<?php foreach($list as $item) { ?>
 				
-					<li class="oval"><a href="#"><img src="img\<?php echo $item["image"] ?> "width="144" height="144"/></a></li>
-					
-				<?php } ?>
-				<?php } ?>
+				
             </ul>
           </figure>
         </article>
